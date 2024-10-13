7425d0ba6e46a4bbf43cac6bc030cdd1e09077d4
taranb27 <taranboodhoo@gmail.com>
2024-10-13 15:33:06 +0400

    Added Checkout form

---
+++ subjects.js
@@ -0,0 +1,37 @@
+let subjects = [
+    {
+        id: '001',
+        Subject: 'Maths',
+        Location: 'London',
+        Price: '£100',
+        Spaces: 5,
+    },
+    {
+        id: '002',
+        Subject: 'Maths',
+        Location: 'Oxford',
+        Price: '£100',
+        Spaces: 5,
+    },
+    {
+        id: '003',
+        Subject: 'English',
+        Location: 'London',
+        Price: '£100',
+        Spaces: 5,
+    },
+    {
+        id: '004',
+        Subject: 'English',
+        Location: 'York',
+        Price: '£80',
+        Spaces: 5,
+    },
+    {
+        id: '005',
+        Subject: 'Music',
+        Location: 'Bristol',
+        Price: '£90',
+        Spaces: 5,
+    },
+]
