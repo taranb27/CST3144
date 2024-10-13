2b1646521a76e8856a428a2b74e8afd68bed1a26
taranb27 <taranboodhoo@gmail.com>
2024-10-13 20:45:30 +0400

    added code to fetch and display from mongodb

--- server.js
+++ server.js
@@ -1,93 +1,102 @@
-// const express = require("express")
-// const session = require("express-session")
-// const path = require("path")
-// const bodyParser = require("body-parser")
-// const {MongoClient, ServerApiVersion} = require("mongodb");
-// const app = express()
-// app.use(bodyParser.json())
+const express = require("express")
+const session = require("express-session")
+const path = require("path")
+const bodyParser = require("body-parser")
+const {MongoClient, ServerApiVersion} = require("mongodb");
+const app = express()
+app.use(bodyParser.json())
 
-// const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
-// const server = "cluster0.2ior5mc.mongodb.net";
+const server = "cluster0.2ior5mc.mongodb.net";
 
-// const encodedusername = encodeURIComponent("tb848");
-// const encodedpwd = encodeURIComponent("CST3144");
+const encodedusername = encodeURIComponent("tb848");
+const encodedpwd = encodeURIComponent("CST3144");
 
-// const URI = `mongodb+srv://${encodedusername}:${encodedpwd}@${server}/?retryWrites=true&w=majority&appName=Cluster0;`;
+const URI = `mongodb+srv://${encodedusername}:${encodedpwd}@${server}/?retryWrites=true&w=majority&appName=Cluster0;`;
 
-// const client = new MongoClient(URI, {
-//     serverApi: {
-//         version: ServerApiVersion.v1,
-//         strict: false,
-//         deprecationErrors: true,
-//     }
-// });
+const client = new MongoClient(URI, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: false,
+        deprecationErrors: true,
+    }
+});
 
-// client.connect()
-//     .then(() => {
-//         console.log("connected to db");
-//         const database = client.db("CST3144");
-//         const collection = database.collection("course_details");
+client.connect()
+    .then(() => {
+        console.log("connected to db");
+        const database = client.db("CST3144");
+        const collection = database.collection("course_details");
 
-//         // The function added courses to the database, if the courses exists, nothing is added
-//         async function courses_details(){
-//             const courseDetails = [
-//                 {Subject: "Maths", Location: "London", Price: "£100"},
-//                 {Subject: "English", Location: "Bristol", Price: "£80"},
-//                 {Subject: "French", Location: "York", Price: "£90"},
-//                 {Subject: "Science", Location: "London", Price: "£120"},
-//                 {Subject: "Maths", Location: "York", Price: "£100"},
-//                 {Subject: "Music", Location: "Bristol", Price: "£80"},
-//                 {Subject: "English", Location: "London", Price: "£80"}
-//             ];
+        // The function added courses to the database, if the courses exists, nothing is added
+        async function courses_details(){
+            const courseDetails = [
+                {Subject: "Maths", Location: "London", Price: "£100"},
+                {Subject: "English", Location: "Bristol", Price: "£80"},
+                {Subject: "French", Location: "York", Price: "£90"},
+                {Subject: "Science", Location: "London", Price: "£120"},
+                {Subject: "Maths", Location: "York", Price: "£100"},
+                {Subject: "Music", Location: "Bristol", Price: "£80"},
+                {Subject: "English", Location: "London", Price: "£80"},
+                {Subject: "Maths", Location: "York", Price: "£1020"},
+                {Subject: "Science", Location: "Liverpool", Price: "£85"},
+                {Subject: "Frech", Location: "Manchester", Price: "£86"}
+            ];
 
-//             for (let course of courseDetails) {
-//                 const existingCourse = await collection.findOne({
-//                     Subject: course.Subject,
-//                     Location: course.Location,
-//                     Price: course.Price
-//                 });
-//                 if(!existingCourse) {
-//                     await collection.insertOne(course);
-//                     console.log("New course added");
-//                 } else {
-//                     console.log("courses already exists");
-//                 }
-//             }
-//             await client.close();
-//         }
+            for (let course of courseDetails) {
+                const existingCourse = await collection.findOne({
+                    Subject: course.Subject,
+                    Location: course.Location,
+                    Price: course.Price
+                });
+                if(!existingCourse) {
+                    await collection.insertOne(course);
+                    console.log("New course added");
+                } else {
+                    console.log("courses already exists");
+                }
+            }
+            await client.close();
+        }
 
-//         courses_details();
+        courses_details();
 
-//         app.use(express.static(path.join(__dirname)));
+        app.use(express.static(path.join(__dirname)));
 
-//         app.get('/', (req, res) => {
-//             res.sendFile(path.join(__dirname, 'index.html'));
-//         });
+        app.get('/', (req, res) => {
+            res.sendFile(path.join(__dirname, 'index.html'));
+        });
 
-//         // Sends the course details from MongoDB to the frontend
-//         app.get('/courses', async (req, res) => {
-//             try {
-//                 const courses = await collection.find({}).toArray();
-//                 console.log(courses)
-//                 res.send(JSON.stringify(courses));
-//                 // res.status(200).json(courses);
-//                 await client.close();
-//             } catch (err) {
-//                 console.error("Failed", err);
-//                 res.status(500).send("Error fetching courses");
-//             }
-//         });
+        // Sends the course details from MongoDB to the frontend
+        app.get('/courses', async (req, res) => {
+            try {
+                // console.log("working")
+                await client.connect();
+                const db1 = client.db("CST3144");
+                const collection1 = db1.collection("course_details");
 
-//         // async function find() {
-//         //     const courses = await collection.find({}).toArray();
-//         //     console.log(courses);
-//         //     await client.close();
-//         // }
-//         // find();
+                const courses = await collection1.find().toArray();
+                // console.log(courses)
+                res.json(courses)
+                await client.close();
+            } catch (err) {
+                console.error("Failed", err);
+                res.status(500).send("Error fetching courses");
+            } finally {
+                await client.close();
+            }
+        });
+
+        // async function find() {
+        //     const courses = await collection.find({}).toArray();
+        //     console.log(courses);
+        //     await client.close();
+        // }
+        // find();
         
-//         app.use(express.json());
+        app.use(express.json());
 
-//         app.listen(PORT, () => console.log('server running'));
-//     })
-//     .catch(err => console.error("error connecting to db", err));
+        app.listen(PORT, () => console.log('server running'));
+    })
+    .catch(err => console.error("error connecting to db", err));
