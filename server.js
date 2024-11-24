23ac1adb904204fbac5ff5751f38598e3f883e6d
taranb27 <taranboodhoo@gmail.com>
2024-11-24 22:20:33 +0400

    cors

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(bodyParser.json());
 app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname)));
 app.use('/images', express.static(path.join(__dirname, 'images')));
-// app.use(cors({origin: ["https://taranb27.github.io/frontend/"]}));
+app.use(cors({origin: ["https://taranb27.github.io/frontend/"]}));
 
 
 client.connect()
